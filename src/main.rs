bcc0f9d676ab0c09b821e448722e8e1d0d86cf03
NotAlfred <mohammadalitalebi2001@gmail.com>
2024-06-29 19:18:21 +0330

    wrote the first part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    
+use std::{
+    io,
+    net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket},
+};
+
+use nix::sys::socket::{listen, Backlog};
+
+fn main() -> Result<(), io::Error> {
+    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+
+    let socket = UdpSocket::bind(&socket_addr)?;
+    let backlog = Backlog::new(1)?;
+
+    listen(&socket, backlog)?;
+
+    let mut buf = [0; 1024];
+    loop {
+        match socket.recv(&mut buf) {
+            Ok(received) => println!("{} recieved.\nmesseage:\n{:?}", received, &buf[..received]),
+            Err(_) => break,
+        }
+    }
+
+    Ok(())
 }
