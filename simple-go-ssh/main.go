c2fdc1c9fbf1ba35e301a5a269a514590f8ad9b0
NotAlfred <mohammadalitalebi2001@gmail.com>
2024-06-30 17:34:48 +0330

    added basic network capabilites

--- simple-go-ssh/main.go
+++ simple-go-ssh/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"image/color"
 	"log"
+	"net"
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/text"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
 func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:5555")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	go func() {
 		window := new(app.Window)
-		err := run(window)
+		err := run(window, &conn)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -23,11 +32,32 @@ func main() {
 	}()
 
 	app.Main()
+
+	conn.Close()
 }
 
-func run(window *app.Window) error {
+func send_command(conn *net.Conn, command string) string {
+	fmt.Fprint(*conn, command)
+
+	buffer := make([]byte, 4096)
+	_, err := bufio.NewReader(*conn).Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(buffer)
+}
+
+func run(window *app.Window, conn *net.Conn) error {
 	theme := material.NewTheme()
+
 	var ops op.Ops
+
+	var editor widget.Editor
+	editor.Submit = true
+
+	var clickable widget.Clickable
+
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
@@ -36,10 +66,34 @@ func run(window *app.Window) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 
-			title := material.H1(theme, "PTY")
-			title.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
-			title.Alignment = text.Middle
-			title.Layout(gtx)
+			if clickable.Clicked(gtx) {
+				result := send_command(conn, editor.Text())
+				editor.SetText(result)
+			}
+
+			layout.Flex{
+				Axis: layout.Horizontal, Alignment: layout.End, Spacing: layout.SpaceEnd,
+			}.Layout(gtx, layout.Rigid(
+				func(gtx layout.Context) layout.Dimensions {
+
+					editor_style := material.Editor(theme, &editor, "editor")
+					editor_style.Layout(gtx)
+					editor_style.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
+
+					return editor_style.Layout(gtx)
+				}))
+
+			layout.Flex{
+				Axis: layout.Vertical, Alignment: layout.End, Spacing: layout.SpaceStart,
+			}.Layout(gtx, layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+
+				clickable_style := material.Button(theme, &clickable, "send_button")
+				clickable_style.Color = color.NRGBA{R: 0, B: 0, G: 0, A: 255}
+				clickable_style.Background = color.NRGBA{R: 150, G: 0, B: 100, A: 255}
+				clickable_style.CornerRadius = 2
+
+				return clickable_style.Layout(gtx)
+			}))
 
 			e.Frame(gtx.Ops)
 		}
